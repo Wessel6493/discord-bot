31c8fab68c075543e8fdf7f9dfdac576ee3085e6
Wessel6493 <wessellaks36@gmail.com>
2025-09-26 09:58:37 +0200

    Update default port for Flask app to 5000

--- bot.py
+++ bot.py
@@ -185,7 +185,7 @@ def home():
     return "Buddy Bot is online!"
 
 def run():
-    port = int(os.environ.get("PORT", 3000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
